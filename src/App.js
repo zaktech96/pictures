f39ea9de2ef365be26d3676762b1d094c4bdff1a
zaktech96 <zakariyesahid96@gmail.com>
2023-07-03 00:23:37 +0100

    project set up

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
+import SearchBar from "./components/SearchBar";
+
 function App() {
-  return <div> App</div>;
+  const handleSubmit = (term) => {
+    // event button for search bar
+    console.log("do search with", term);
+  };
+  return (
+    <div>
+      {/*  this shows searchBar component, 
+      event handler can be any name in this case for components*/}
+      <SearchBar onSubmit={handleSubmit} />
+    </div>
+  );
 }
 
 export default App;
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,5 +1,29 @@
-function SearchBar() {
-  return <div> SearchBar</div>;
+import { useState } from "react";
+
+function SearchBar({ onSubmit }) {
+  const [term, setTerm] = useState(""); // step 1 new piece of state
+  const handleFormSubmit = (event) => {
+    event.preventDefault(); // this helps to prevent reloading process
+
+    onSubmit("trees"); //call what ever text user types in input
+  };
+
+  const handleChange = (event) => {
+    // step 2 onChange event
+    // console.log(event.target.value); // step 3 gets value from input
+    setTerm(event.target.value); // gets value and updates state
+  };
+  return (
+    <div>
+      <form onSubmit={handleFormSubmit}>
+        {/* shows event to form*/}
+        {/*  wrapping form around input, makes a submit event*/}
+        <input value={term} onChange={handleChange} />{" "}
+        {/* step 3get value from input */}
+        {/* step 5 pass state to input value prop */}
+      </form>
+    </div>
+  );
 }
 
 export default SearchBar;
